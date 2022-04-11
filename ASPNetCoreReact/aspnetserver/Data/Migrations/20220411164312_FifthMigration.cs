389dba1ab9e120db0ce7153e1e42870a2bf03354
Mahmoud <mahmoud.abdelaty@ucalgary.ca>
2022-04-11 20:46:13 +0400

    Add files via upload

---
+++ ASPNetCoreReact/aspnetserver/Data/Migrations/20220411164312_FifthMigration.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace aspnetserver.Data.Migrations
+{
+    public partial class FifthMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Carts",
+                columns: table => new
+                {
+                    CartID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    cartItems = table.Column<string>(type: "TEXT", maxLength: 999999, nullable: false),
+                    BuyerID = table.Column<int>(type: "INTEGER", maxLength: 9999, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Carts", x => x.CartID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Carts");
+        }
+    }
+}
