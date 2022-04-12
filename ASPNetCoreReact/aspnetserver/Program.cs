807d9fb03793a5a80e1d7563d3530ddb52be90cb
Mahmoud <mahmoud.abdelaty@ucalgary.ca>
2022-04-12 17:14:05 +0400

    Add files via upload

--- ASPNetCoreReact/aspnetserver/Program.cs
+++ ASPNetCoreReact/aspnetserver/Program.cs
@@ -10,7 +10,7 @@ builder.Services.AddCors(options =>
         {
             builder.AllowAnyMethod()
                    .AllowAnyHeader()
-                   .WithOrigins("http://localhost:3000", "https://appname.azurestaticapps.net");
+                   .AllowAnyOrigin();
         }
     );
 });
